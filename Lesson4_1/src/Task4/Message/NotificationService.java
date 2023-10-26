dee01a1f00245c7d2292724719e72c726e2ea921
Stan Bad <stankv@mail.ru>
2023-10-26 19:02:26 +0300

    Mockito: проверка сервиса отправки сообщений.

---
+++ Lesson4_1/src/Task4/Message/NotificationService.java
@@ -0,0 +1,13 @@
+package Task4.Message;
+
+public class NotificationService {
+    private MessageService messageService;
+
+    public NotificationService(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    public void sendNotification(String message, String recipient) {
+        messageService.sendMessage(message, recipient);
+    }
+}
